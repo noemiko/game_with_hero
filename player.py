f016412071d5a84c9a8d987657db9e74a3e3818a
noemiko <noemikow8@gmail.com>
2019-12-30 16:59:19 +0100

    upgrade image

--- player.py
+++ player.py
@@ -1,42 +1,28 @@
-#!/usr/bin/env python3
-# draw a world
-# add a player and player control
-# add player movement
-
-# GNU All-Permissive License
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
-
 import pygame
 import sys
 import os
 
-'''
-Objects
-'''
 
 class Player(pygame.sprite.Sprite):
     '''
     Spawn a player
     '''
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.movex = 0
         self.movey = 0
         self.frame = 0
         self.images = []
-        for i in range(1,5):
-            img = pygame.image.load(os.path.join('images','walk_rar' + str(i) + '.png'))
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '_kol_filled.png'))
             img = pygame.transform.scale(img, (200, 300))
             img.convert_alpha()
-            img.set_colorkey(WHITE)
             self.images.append(img)
             self.image = self.images[0]
-            self.rect  = self.image.get_rect()
+            self.rect = self.image.get_rect()
 
-    def control(self,x,y):
+    def control(self, x, y):
         '''
         control player movement
         '''
@@ -54,16 +40,16 @@ class Player(pygame.sprite.Sprite):
         # moving left
         if self.movex < 0:
             self.frame += 1
-            if self.frame > 3*ani:
+            if self.frame > 3 * ani:
                 self.frame = 1
-            self.image = self.images[self.frame//ani]
+            self.image = self.images[self.frame // ani]
 
         # moving right
         if self.movex > 0:
             self.frame += 1
-            if self.frame > 3*ani:
+            if self.frame > 3 * ani:
                 self.frame = 1
-            self.image = self.images[(self.frame//ani)]
+            self.image = self.images[(self.frame // ani)]
 
 
 '''
@@ -72,26 +58,26 @@ Setup
 worldx = 850
 worldy = 480
 
-fps = 40        # frame rate
-ani = 2        # animation cycles
+fps = 40  # frame rate
+ani = 2  # animation cycles
 clock = pygame.time.Clock()
 pygame.init()
 main = True
 
-BLUE  = (25,25,200)
-BLACK = (23,23,23 )
-WHITE = (254,254,254)
-ALPHA = (0,255,0)
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
 
-world = pygame.display.set_mode([worldx,worldy])
-backdrop = pygame.image.load(os.path.join('images','bg.png'))
+world = pygame.display.set_mode([worldx, worldy])
+backdrop = pygame.image.load(os.path.join('images', 'bg.png'))
 backdropbox = world.get_rect()
-player = Player()   # spawn player
+player = Player()  # spawn player
 player.rect.x = 0
 player.rect.y = 150
 player_list = pygame.sprite.Group()
 player_list.add(player)
-steps = 30      # how fast to move
+steps = 30  # how fast to move
 
 '''
 Main loop
@@ -99,22 +85,23 @@ Main loop
 while main == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
+            pygame.quit();
+            sys.exit()
             main = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(-steps,0)
+                player.control(-steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(steps,0)
+                player.control(steps, 0)
             if event.key == pygame.K_UP or event.key == ord('w'):
                 print('jump')
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(steps,0)
+                player.control(steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(-steps,0)
+                player.control(-steps, 0)
             if event.key == ord('q'):
                 pygame.quit()
                 sys.exit()
@@ -123,6 +110,6 @@ while main == True:
         world.fill(WHITE)
         world.blit(backdrop, backdropbox)
         player.update()
-        player_list.draw(world) #refresh player position
+        player_list.draw(world)  # refresh player position
         pygame.display.flip()
         clock.tick(fps)
