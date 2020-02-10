4dba3575ee0ba64b86afaf677fde5779b54f8dd4
n.kowalewska <n.kowalewska@clearcode.cc>
2020-02-10 09:47:30 +0100

    refactgor

--- game.py
+++ game.py
@@ -1,59 +1,70 @@
 import pygame
-import sys
 import os
+import sys
 
 from player import Player
 from settings import worldx, worldy
 from settings import WHITE
 from settings import fps
-
-"""
-Setup
-"""
-clock = pygame.time.Clock()
-pygame.init()
-main = True
-
-world = pygame.display.set_mode([worldx, worldy])
-backdrop = pygame.image.load(os.path.join("images", "bg.png"))
-backdropbox = world.get_rect()
-player = Player(0, 150)  # spawn player
-
-player_list = pygame.sprite.Group()
-player_list.add(player)
-steps = 30  # how fast to move
+from settings import walk_speed
 
 """
 Main loop
 """
 
-while main:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == ord("a"):
-                player.control(-steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord("d"):
-                player.control(steps, 0)
-            if event.key == pygame.K_UP or event.key == ord("w"):
-                print("jump")
+class Game:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        pygame.init()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == ord("a"):
-                player.control(steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord("d"):
-                player.control(-steps, 0)
-            if event.key == ord("q") or event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                print("Quit")
-                sys.exit()
+    def setup(self):
+        self.world = pygame.display.set_mode([worldx, worldy])
+        self.background = pygame.image.load(os.path.join("images", "bg.png"))
+        self.backdropbox = self.world.get_rect()
+        self.player = Player(0, 150)  # spawn player
 
-        world.fill(WHITE)
-        world.blit(backdrop, backdropbox)
-        player.update()
-        player_list.draw(world)  # refresh player position
+        self.player_list = pygame.sprite.Group()
+        self.player_list.add(self.player)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT or event.key == ord("a"):
+                        self.player.control(-walk_speed, -1)
+                    if event.key == pygame.K_RIGHT or event.key == ord("d"):
+                        self.player.control(walk_speed, -1)
+                    if event.key == pygame.K_UP or event.key == ord("w"):
+                        print("jump")
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == ord("a"):
+                        self.player.control(walk_speed, -1)
+                    if event.key == pygame.K_RIGHT or event.key == ord("d"):
+                        self.player.control(-walk_speed, -1)
+                    if event.key == ord("q") or event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        print("Quit")
+                        sys.exit()
+                self.draw_world()
+
+    def handle_controlls(self):
+
+    def draw_world(self):
+        self.world.fill(WHITE)
+        self.world.blit(self.background, self.backdropbox)
+        self.player.update()
+        self.player_list.draw(self.world)  # refresh player position
         pygame.display.flip()
-        clock.tick(fps)
+        self.clock.tick(fps)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.setup()
+    game.run()
--- settings.py
+++ settings.py
@@ -4,6 +4,8 @@ worldy = 480
 fps = 40  # frame rate
 animation_cycles = 2  # animation cycles
 
+walk_speed = 30  # how fast to move
+
 BLUE = (25, 25, 200)
 BLACK = (23, 23, 23)
 WHITE = (254, 254, 254)
