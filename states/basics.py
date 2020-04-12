c4a56c6259f6fa714ee65e35c8a75c04fffe6611
n.kowalewska <n.kowalewska@clearcode.cc>
2020-04-12 14:25:11 +0200

    dont allow user to set empy nickname

--- states/basics.py
+++ states/basics.py
@@ -15,31 +15,29 @@ class NameInput(States):
     def __init__(self):
         States.__init__(self)
         self.next = "menu"
-        self.COLOR_INACTIVE = pg.Color('lightskyblue1')
-        self.COLOR_ACTIVE = pg.Color('black')
+        self.color = pg.Color('black')
         self.FONT = pg.font.Font('freesansbold.ttf', 30)
         self.rect = pg.Rect(world_width / 2 - 250, world_heigh / 2, 50, 50)
-        self.color = self.COLOR_INACTIVE
         self.text = ""
         self.txt_surface = self.FONT.render(self.text, True, self.color)
-        self.active = True
 
     def startup(self):
         print("starting Game state stuff")
 
     def get_event(self, event):
         if event.type == pg.KEYDOWN:
-            if self.active:
-                if event.key == pg.K_RETURN:
-                    if self.text:
-                        globals.nickname = self.text
-                    self.done = True
+            if event.key == pg.K_RETURN:
+                if not self.text:
+                    self.color = pg.Color("red")
+                    return
+                globals.nickname = self.text
+                self.done = True
 
-                elif event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    if len(self.text) != nickname_max_length:
-                        self.text += event.unicode
+            elif event.key == pg.K_BACKSPACE:
+                self.text = self.text[:-1]
+            else:
+                if len(self.text) != nickname_max_length:
+                    self.text += event.unicode
                 # Re-render the text.
 
     def update(self, screen, deltatime):
@@ -51,7 +49,7 @@ class NameInput(States):
     def draw(self, screen):
         screen.fill(WHITE)
         screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
-        text_surface = self.FONT.render("Input your nickname", True, BLACK)
+        text_surface = self.FONT.render("Input your nickname", True, self.color)
         rect = text_surface.get_rect()
         rect.center = (world_width / 2 - 50, world_heigh / 2 - 50)
         screen.blit(text_surface, rect)
