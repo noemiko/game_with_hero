e11eb54406ba278312fb0e9f8e126b449aaa40e4
n.kowalewska <n.kowalewska@clearcode.cc>
2020-05-24 14:14:22 +0200

    add gravity and cosmo suit

--- src/duchshund_walk/states/game/player.py
+++ src/duchshund_walk/states/game/player.py
@@ -5,6 +5,7 @@ from duchshund_walk.utils import get_dog_image_folder
 from duchshund_walk.utils import get_human_image_folder
 from duchshund_walk.utils import get_images
 from duchshund_walk.utils import scale_images
+from PIL import Image
 
 
 class Bullet(pg.sprite.Sprite):
@@ -36,6 +37,8 @@ class Player(pg.sprite.Sprite):
         self.movey = 0
         self.is_jumping = False
         self.health = 100
+        self.g = 10
+        self.jump_velocity = 0
 
     def load_images(self):
         pass
@@ -47,11 +50,6 @@ class Player(pg.sprite.Sprite):
 
     def is_on_the_top(self):
         if self.rect.y <= self.jump_heigh:
-            return True
-        return False
-
-    def is_during_jump(self):
-        if self.rect.y >= self.jump_heigh and self.rect.y < self.ground_level:
             return True
         return False
 
@@ -70,14 +68,40 @@ class Duchshund(Player):
         bullet = Bullet(self.rect.x + 80, self.rect.y + 25)
         self.bullets.append(bullet)
 
+    def cosmos_mode(self):
+        self.g = 5
+        self.wear_cosmo_suit()
+
+    def earth_mode(self):
+        self.g = 10
+        self.images_frames = self.load_images()
+
     def load_images(self):
         images_folder = get_dog_image_folder()
         images = get_images(images_folder)
         return scale_images(images, (100, 50))
 
+    def wear_cosmo_suit(self):
+        folder = get_dog_image_folder()
+        images = get_images(folder)
+
+        strFormat = "RGBA"
+        images_with_helmet = []
+        for current_image in images:
+            raw_str = pg.image.tostring(current_image, strFormat, False)
+            im1 = Image.frombytes(strFormat, current_image.get_size(), raw_str)
+            im2 = Image.open("src/duchshund_walk/states/game/images/cosmos/dachshund_cosmo.png")
+            new_image = Image.alpha_composite(im1, im2)
+            myImage = pg.image.fromstring(new_image.tobytes(), new_image.size, new_image.mode)
+            images_with_helmet.append(myImage)
+
+        images = scale_images(images_with_helmet, (100, 50))
+        self.images_frames = images
+
     def jump(self):
         if self.is_on_the_ground():
             self.movey -= 100
+            self.jump_velocity = 50
             self.is_jumping = True
 
     def get_event(self, event):
@@ -92,21 +116,20 @@ class Duchshund(Player):
         Update sprite position
         """
 
-        slowdown = 300.0
-        player_speed = game_deltatime / slowdown
-        if self.is_on_the_top():
-            self.movey += 100
+        if self.is_jumping:
+            print("dog is during jump")
+            self.rect.y -= self.jump_velocity
+            self.jump_velocity -= self.g
+
+        if self.is_on_the_ground():
+            self.jump_velocity = 0
+            self.rect.y = self.ground_level
             self.is_jumping = False
 
-        if self.is_on_the_ground() and not self.is_jumping:
-            self.movey = 0
-            self.rect.y = self.ground_level
-
-        self.rect.y += self.movey * player_speed
         self.rect.topleft = self.rect.x, self.rect.y
 
         self.frame += 1
-        if self.is_jumping or self.is_during_jump():
+        if self.is_jumping:
             self.frame = 0
             self.image = self.images_frames[4]
             return
@@ -124,11 +147,36 @@ class Human(Player):
         Player.__init__(self, x, y)
         self.jump_heigh = 100
         self.ground_level = y
-
         self.bullets = []
+
+    def cosmos_mode(self):
+        self.g = 6
+        self.wear_cosmo_suit()
+
+    def earth_mode(self):
+        self.g = 10
+        self.images_frames = self.load_images()
+
+    def wear_cosmo_suit(self):
+        folder = get_human_image_folder()
+        images = get_images(folder)
+
+        strFormat = "RGBA"
+        images_with_helmet = []
+        for current_image in images:
+            raw_str = pg.image.tostring(current_image, strFormat, False)
+            im1 = Image.frombytes(strFormat, current_image.get_size(), raw_str)
+            im2 = Image.open("src/duchshund_walk/states/game/images/cosmos/cosmo_helmet.png")
+            new_image = Image.alpha_composite(im1, im2)
+            myImage = pg.image.fromstring(new_image.tobytes(), new_image.size, new_image.mode)
+            images_with_helmet.append(myImage)
+
+        images = scale_images(images_with_helmet, (150, 200))
+        self.images_frames = images
 
     def jump(self):
         if self.is_on_the_ground():
+            self.jump_velocity = 50
             self.movey -= 100
             self.is_jumping = True
 
@@ -152,21 +200,15 @@ class Human(Player):
     def update(self, game_deltatime):
         if self.is_jumping:
             print("human is during jump")
-            slowdown = 300.0
-            player_speed = game_deltatime / slowdown
-            self.rect.y += self.movey * player_speed
-
-        if self.is_on_the_top():
-            self.movey += 100
+            self.rect.y -= self.jump_velocity
+            self.jump_velocity -= self.g
 
         if self.is_on_the_ground():
+            self.jump_velocity = 0
+            self.rect.y = self.ground_level
             self.is_jumping = False
 
-        if self.is_on_the_ground() and not self.is_jumping:
-            self.movey = 0
-            self.rect.y = self.ground_level
-
-        if self.is_jumping or self.is_during_jump():
+        if self.is_jumping:
             self.image = self.images_frames[2]
             self.frame = 0
             return
--- src/run.py
+++ src/run.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         "options": Options,
         "scores": Scores,
     }
-    APP.setup_states(STATE_DICT, "nickname")
+    APP.setup_states(STATE_DICT, "menu")
     APP.main_game_loop()
     pg.quit()
     sys.exit()
